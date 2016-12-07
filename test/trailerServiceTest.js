657e926ed67b663c35b4e321d72981cd7a07ccd1
Carlos Mateos <carlos.mateos@mynewsdesk.com>
2016-12-07 01:55:39 +0100

    Renames test/service to test/trailerServiceTest
    Uses as well the new mocks

--- test/trailerServiceTest.js
+++ test/trailerServiceTest.js
@@ -1,30 +1,27 @@
-const assert = require('assert');
 const request = require('superagent');
 const expect = require('chai').expect;
 
 const app = require('../app');
-const fetchTrailer = require('../routes/trailer_service');
+const fetchTrailer = require('../routes/trailerService');
 
-const viaplayFixture = require('./viaplayFixture');
-const imdbFixture = require('./imdbFixture');
+const config = require('./superagentMocks');
 
 describe('trailerService', () => {
-  let viaplayMock, imdbMock;
+  let superagentMocks;
 
   before(() => {
-    viaplayMock = require('superagent-mock')(request, viaplayFixture);
-    imdbMock = require('superagent-mock')(request, imdbFixture);
+    superagentMocks = require('superagent-mock')(request, config);
   });
 
   after(() => {
-    viaplayMock.unset();
-    imdbMock.unset();
+    superagentMocks.unset();
   });
 
   it('returns a link to a trailer', (done) => {
     fetchTrailer('https://content.viaplay.se/pc-se/film/ted-2-2015', (err, res) => {
       if (err) return done(err);
 
+      expect(err).to.be.empty;
       expect(res.site).to.eq('Youtube')
       expect(res.url).to.not.be.empty;
 
